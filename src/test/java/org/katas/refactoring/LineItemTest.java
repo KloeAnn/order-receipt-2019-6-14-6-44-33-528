3399f4b89da9c853f4378287e7a38b06b1c0ec28
KloeAnn <201530611012@mail.scut.edu.cn>
2019-07-15 20:35:20 +0800

    [Kloe][feat:test:should_return_price_when_get_price&test:should_return_quantity_when_get_quantity&test:should_return_price_when_get_price ]

---
+++ src/test/java/org/katas/refactoring/LineItemTest.java
@@ -0,0 +1,39 @@
+package org.katas.refactoring;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LineItemTest {
+
+    @Test
+    public void should_return_description_when_get_description(){
+        //given
+        LineItem lineItem=new LineItem("Some description",1.22,1);
+        //when
+        String description=lineItem.getDescription();
+        //then
+        assertSame("Some description",description);
+    }
+
+    @Test
+    public void should_return_price_when_get_price(){
+        //given
+        LineItem lineItem=new LineItem("Some description",1.22,1);
+        //when
+        double price=lineItem.getPrice();
+        //then
+        assertEquals(1.22,price);
+    }
+
+    @Test
+    public void should_return_quantity_when_get_quantity(){
+        //given
+        LineItem lineItem=new LineItem("Some description",1.22,1);
+        //when
+        int quantity=lineItem.getQuantity();
+        //then
+        assertEquals(1,quantity);
+    }
+
+}
---
+++ src/test/java/org/katas/refactoring/OrderTest.java
@@ -0,0 +1,20 @@
+package org.katas.refactoring;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderTest {
+
+    @Test
+    void getCustomerName() {
+    }
+
+    @Test
+    void getCustomerAddress() {
+    }
+
+    @Test
+    void getLineItems() {
+    }
+}

6308abc66b3e1372d81006adc5676a133039e3d4
KloeAnn <201530611012@mail.scut.edu.cn>
2019-07-15 21:18:08 +0800

    [Kloe][feat:test:should_return_customer_name_when_get_customer_name&test:should_return_customer_address_when_get_customer_address&test:should_return_line_item_when_get__line_item]

--- src/test/java/org/katas/refactoring/OrderTest.java
+++ src/test/java/org/katas/refactoring/OrderTest.java
@@ -2,19 +2,50 @@ package org.katas.refactoring;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderTest {
 
+
     @Test
-    void getCustomerName() {
+    void should_return_customer_name_when_get_customer_name() {
+        //given
+        LineItem lineItem=new LineItem("Some description",1.22,1);
+        List<LineItem> list=new ArrayList<LineItem>();
+        list.add(lineItem);
+        Order order=new Order("Kloe","Beijing",list);
+        //when
+        String name=order.getCustomerName();
+        //then
+        assertSame("Kloe",name);
     }
 
     @Test
-    void getCustomerAddress() {
+    public void should_return_customer_address_when_get_customer_address() {
+        //given
+        LineItem lineItem=new LineItem("Some description",1.22,1);
+        List<LineItem> list=new ArrayList<LineItem>();
+        list.add(lineItem);
+        Order order=new Order("Kloe","Beijing",list);
+        //when
+        String address=order.getCustomerAddress();
+        //then
+        assertSame("Beijing",address);
     }
 
     @Test
-    void getLineItems() {
+    public void should_return_line_item_when_get__line_item() {
+        //given
+        LineItem lineItem=new LineItem("Some description",1.22,1);
+        List<LineItem> list=new ArrayList<LineItem>();
+        list.add(lineItem);
+        Order order=new Order("Kloe","Beijing",list);
+        //when
+        List res=order.getLineItems();
+        //then
+        assertSame(list,res);
     }
 }
